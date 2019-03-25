64d597f4e29ad8b45faf6fb029319d8e554028d1
hideya satake <yugiohocg123@gmail.com>
2019-03-25 17:08:30 +0900

    a

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,24 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(){
+    var datenow = new Date();
+    var yearnow = datenow.getFullYear();
+    var monthnow = datenow.getMonth();
+    var firstday = new Date(yearnow, monthnow, 1);
+    
 
-}
 
-window.onload = function(){
-  for(var j=2; j<=6; j++){
-    var date:HTMLElement = <HTMLElement>document.createElement("ion-row");
-    var week:HTMLElement = <HTMLElement>document.getElementById("calender");
+    //画面に表示
+    window.onload = function(){
+      for(var j=1; j<=5; j++){
+        var date:HTMLElement = <HTMLElement>document.createElement("ion-row");
+        var week:HTMLElement = <HTMLElement>document.getElementById("calender");
 
-    date.setAttribute("id", "week"+j);
-    date.classList.add("week");
-    week.appendChild(date);
-
-    for(var i=0; i<=6; i++){
-      var week2:HTMLElement = <HTMLElement>document.getElementById("week"+j);
-      var day:HTMLElement = <HTMLElement>document.createElement("ion-col");
-
-      day.textContent = i.toString();
-      week2.appendChild(day);
+        date.setAttribute("id", "week"+j);
+        date.classList.add("week");
+        week.appendChild(date);
+    
+        for(var i=0; i<=6; i++){
+          var weeknum:HTMLElement = <HTMLElement>document.getElementById("week"+j);
+          var day:HTMLElement = <HTMLElement>document.createElement("ion-col");
+          var span:HTMLElement = <HTMLElement>document.createElement("span");
+    
+          span.textContent = i.toString();
+          day.classList.add(i.toString());
+          weeknum.appendChild(day);
+          day.appendChild(span);
+        }
+      }
+      console.log(firstday.getDay());
     }
   }
+}
+
+function getcalender(year, month){
+
 }
