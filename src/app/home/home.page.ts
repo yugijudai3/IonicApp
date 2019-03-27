03ff5818ede26ec4c5e4f3d823f1286595ba01a2
hideya satake <yugiohocg123@gmail.com>
2019-03-27 17:25:49 +0900

    a

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,48 +7,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   constructor(){
-    var datenow = new Date();
-    var yearnow = datenow.getFullYear();
-    var monthnow = datenow.getMonth();
-    var stage = 0;
-
     //画面に表示
     window.onload = function(){
-      for(var j=1; j<=5; j++){
-        var date:HTMLElement = <HTMLElement>document.createElement("ion-row");
-        var week:HTMLElement = <HTMLElement>document.getElementById("calender");
-
-        date.setAttribute("id", "week"+j);
-        date.classList.add("week");
-        week.appendChild(date);
-    
-        for(var i=0; i<=6; i++){
-          var weeknum:HTMLElement = <HTMLElement>document.getElementById("week"+j);
-          var day:HTMLElement = <HTMLElement>document.createElement("ion-col");
-          var span:HTMLElement = <HTMLElement>document.createElement("span");
-          
-          span.classList.add(i.toString());
-          weeknum.appendChild(day);
-          day.appendChild(span);
-        }
-      }
-
-      for(var i=0; i<=30; i++){
-        var daynow = new Date(yearnow, 4, i+1);
-        var select = document.getElementsByClassName(daynow.getDay().toString());
-
-        console.log(daynow);
-        select[stage].textContent = (i+1).toString();
-        
-
-        if(daynow.getDay().toString() == "6"){
-          stage += 1;
-        }
-      }
+      createcalender();
     }
   }
 }
 
-function getcalender(year, month){
+function createcalender(){
+  var datenow = new Date();
+    var yearnow = datenow.getFullYear();
+    var monthnow = datenow.getMonth();
+    var stage = 0;
 
+  for(var j=1; j<=5; j++){
+    var date:HTMLElement = <HTMLElement>document.createElement("ion-row");
+    var week:HTMLElement = <HTMLElement>document.getElementById("calender");
+
+    date.setAttribute("id", "week"+j);
+    date.classList.add("week");
+    week.appendChild(date);
+
+    for(var i=0; i<=6; i++){
+      var weeknum:HTMLElement = <HTMLElement>document.getElementById("week"+j);
+      var day:HTMLElement = <HTMLElement>document.createElement("ion-col");
+      var span:HTMLElement = <HTMLElement>document.createElement("span");
+      
+      span.classList.add(i.toString());
+      weeknum.appendChild(day);
+      day.appendChild(span);
+    }
+  }
+
+  for(var i=0; i<=30; i++){
+    var daynow = new Date(yearnow, 4, i+1);
+    var select = document.getElementsByClassName(daynow.getDay().toString());
+
+    console.log(daynow);
+    select[stage].textContent = (i+1).toString();
+    
+
+    if(daynow.getDay().toString() == "6"){
+      stage += 1;
+    }
+  }
 }
